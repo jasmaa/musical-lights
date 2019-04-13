2fe340c8f82b7e15cead510c3ec328df5730bf64
jasmaa <jasonmaa3955@gmail.com>
2019-04-13 11:12:57 -0400

    Add mode and reconnect buttons

--- android/MusicalLights/app/src/main/java/com/happycampers/musicallights/MainActivity.java
+++ android/MusicalLights/app/src/main/java/com/happycampers/musicallights/MainActivity.java
@@ -20,6 +20,7 @@ import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView outputText;
     private TextView inputText;
     private Button sendButton;
+    private ArrayList<Button> modeButtons;
 
     private final static int REQUEST_ENABLE_BT = 1;
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
@@ -53,14 +55,85 @@ public class MainActivity extends AppCompatActivity {
         outputText = (TextView)findViewById(R.id.output);
         outputText.setMovementMethod(new ScrollingMovementMethod());
 
-        inputText = (TextView) findViewById(R.id.editText);
-
-        sendButton = (Button) findViewById(R.id.sendButton);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        // set button listeners
+        modeButtons = new ArrayList<Button>();
+        modeButtons.add((Button) findViewById(R.id.mode0Btn));
+        modeButtons.add((Button) findViewById(R.id.mode1Btn));
+        modeButtons.add((Button) findViewById(R.id.mode2Btn));
+        modeButtons.add((Button) findViewById(R.id.mode3Btn));
+        modeButtons.add((Button) findViewById(R.id.mode4Btn));
+        modeButtons.add((Button) findViewById(R.id.mode5Btn));
+        modeButtons.get(0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    mSocket.getOutputStream().write((inputText.getText().toString() + "\n").getBytes());
+                    byte[] msg = {0};
+                    mSocket.getOutputStream().write(msg);
+                }
+                catch(IOException e){
+                    Log.d(TAG, "Could not write to stream");
+                    outputText.append("Could not write to steam\n");
+                }
+            }
+        });
+        modeButtons.get(1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    byte[] msg = {1};
+                    mSocket.getOutputStream().write(msg);
+                }
+                catch(IOException e){
+                    Log.d(TAG, "Could not write to stream");
+                    outputText.append("Could not write to steam\n");
+                }
+            }
+        });
+        modeButtons.get(2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    byte[] msg = {2};
+                    mSocket.getOutputStream().write(msg);
+                }
+                catch(IOException e){
+                    Log.d(TAG, "Could not write to stream");
+                    outputText.append("Could not write to steam\n");
+                }
+            }
+        });
+        modeButtons.get(3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    byte[] msg = {3};
+                    mSocket.getOutputStream().write(msg);
+                }
+                catch(IOException e){
+                    Log.d(TAG, "Could not write to stream");
+                    outputText.append("Could not write to steam\n");
+                }
+            }
+        });
+        modeButtons.get(4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    byte[] msg = {4};
+                    mSocket.getOutputStream().write(msg);
+                }
+                catch(IOException e){
+                    Log.d(TAG, "Could not write to stream");
+                    outputText.append("Could not write to steam\n");
+                }
+            }
+        });
+        modeButtons.get(5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    byte[] msg = {5};
+                    mSocket.getOutputStream().write(msg);
                 }
                 catch(IOException e){
                     Log.d(TAG, "Could not write to stream");
@@ -69,7 +142,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ((Button) findViewById((R.id.reconnectBtn))).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btConnect();
+            }
+        });
 
+        // try to connect to bluetooth
+        btConnect();
+    }
+
+    /**
+     * Runs on destroy
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * Connects to ESP32 via Bluetooth
+     */
+    private void btConnect(){
         // check if bluetooth can be used
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         if(adapter == null) {
@@ -81,9 +176,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }
-
-            // get uuid
-            //outputText.append("" + targetDevice.getUuids());
 
             // find target from paired devices
             Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
@@ -125,14 +217,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-    }
-
-    /**
-     * Runs on destroy
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 }
 
